c92545bcc85c15965ee8c13436c74b32b374d22d
van9a <matyushko.vanya.195@gmail.com>
2018-11-13 17:06:53 +0200

    All hovers should have transition(let it be 300ms) console should be cleaned from errors Main screen should have device height

--- app/js/index.js
+++ app/js/index.js
@@ -2,21 +2,33 @@
  * Created by Admin on 31.10.2018.
  */
 $(document).ready(function () {
+    $('.main-screen__hamg-menu').click(function () {
+        $('.main-screen__hamg-menu').toggleClass('active');
+    })
+});
+$(document).ready(function () {
+    $('.main-screen__hamg-menu').click(function () {
+        $('.menu #main-screen__menu').toggleClass('open');
+    })
+});
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("main-screen__menu");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+$(document).ready(function () {
     //initialize swiper when document ready
     var swiper = new Swiper('.swiper-container', {
         pagination: {
             el: '.swiper-pagination',
         },
     });
-    swiper();
 });
-$(document).ready(function () {
-    $('.icon').click(function () {
-        $('.icon').toggleClass('active');
-    })
-});
-$(document).ready(function () {
-    $('.icon').click(function () {
-        $('.menu ul').toggleClass('open');
-    })
-});
+console.clear();
